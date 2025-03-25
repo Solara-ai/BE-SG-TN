4972318c18430a7dc8249b2562c596cf98b511df
do.thang <dowindev26@gmail.com>
2025-03-25 16:16:43 +0700

    SCRUM-77 users/feat/profile: update profile

--- src/main/java/org/se06203/besgtn/service/users/impl/ProfileService.java
+++ src/main/java/org/se06203/besgtn/service/users/impl/ProfileService.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.se06203.besgtn.config.exception.BaseRuntimeException;
 import org.se06203.besgtn.config.exception.ErrorCodeMsg;
 import org.se06203.besgtn.config.security.SecurityUtils;
+import org.se06203.besgtn.dto.request.ProfileRequest;
 import org.se06203.besgtn.dto.response.ProfileResponse;
 import org.se06203.besgtn.persistence.repository.UserRepository;
 import org.se06203.besgtn.utils.mapper.ProfileMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,11 +18,13 @@ import org.springframework.stereotype.Service;
 public class ProfileService {
     private UserRepository userRepository;
     private ProfileMapper profileMapper;
+    private PasswordEncoder passwordEncoder;
 
     @org.springframework.beans.factory.annotation.Autowired
-    public ProfileService(UserRepository userRepository, ProfileMapper profileMapper) {
+    public ProfileService(UserRepository userRepository, ProfileMapper profileMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.profileMapper = profileMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public ProfileResponse getProfile() {
@@ -29,5 +33,21 @@ public class ProfileService {
                 .orElseThrow(() -> new BaseRuntimeException(ErrorCodeMsg.USER_NOT_FOUND));
 
         return profileMapper.mapToProfileResponse(user);
+    }
+
+    public void updateProfile(ProfileRequest profileRequest) {
+        var userId = SecurityUtils.getAuthenticatedUser().getId();
+        var user = userRepository.findById(userId)
+                .orElseThrow(() -> new BaseRuntimeException(ErrorCodeMsg.USER_NOT_FOUND));
+
+        user.setFullName(profileRequest.getFullName());
+        user.setPhone(profileRequest.getPhone());
+        user.setBirthday(profileRequest.getBirthday());
+        user.setGender(profileRequest.getGender());
+        user.setHobbies(profileRequest.getHobbies());
+        user.setOccupation(profileRequest.getOccupation());
+        user.setPassword(passwordEncoder.encode(profileRequest.getPassword()));
+
+        userRepository.save(user);
     }
 }
